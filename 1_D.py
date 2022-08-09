94af2b89b52b420e34c1fea0a041b86591a2a73c
mochi <71626403+yan-motch@users.noreply.github.com>
2022-08-09 11:11:02 +0000

    chnage file structure

--- 1_D.py
+++
@@ -1,7 +0,0 @@
-x = int(46979)
-h = int(x/3600)
-x = x%3600
-m = int(x / 60)
-x = x % 60
-s = x
-print("{}:{}:{}".format(h, m, s))
--- 2_c.py
+++
@@ -1,3 +0,0 @@
-nums = list(map(int, ('10000 1000 100').split()))
-nums.sort()
-print(f"{nums[0]} {nums[1]} {nums[2]}")
